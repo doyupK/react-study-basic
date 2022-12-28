2f74e4be108e5d5d1be49202dcd3730d0bc9d954
doyupKim <gltlvl12@gmail.com>
2022-12-28 15:31:32 +0900

    map

---
+++ src/component/Day.js
@@ -0,0 +1,24 @@
+import dummy from "../db/data.json"
+export default function Day(){
+    const day = 1;
+    const wordList = dummy.words.filter(word => (
+        word.day === day
+    ))
+
+    return <>
+    <table>
+        <tbody>
+        {wordList.map(word=>(
+            <tr>
+                <td>
+                    {word.eng}
+                </td>
+                <td>
+                    {word.kor}
+                </td>
+            </tr>
+            ))}
+        </tbody>
+    </table>
+    </>
+}
