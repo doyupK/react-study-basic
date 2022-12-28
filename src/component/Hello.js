92dbed07eabc16d0269de00457040581a0ec1d42
doyupKim <gltlvl12@gmail.com>
2022-12-28 13:50:55 +0900

    onchange

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -11,6 +11,10 @@ export default function Hello() {
     //     console.log(e.target.value)
     // }
 
+    function showText(txt){
+        console.log(txt)
+    }
+
     return <div>
         <h1>Hello</h1>
         <button onClick={showName}>Show name</button>
@@ -20,11 +24,23 @@ export default function Hello() {
             }
             }>Show age
         </button>
+        {/*방법1*/}
         {/*<input type="text" onChange={showText}/>*/}
+
+        {/*방법 2*/}
+        {/*<input*/}
+        {/*    type="text"*/}
+        {/*    onChange={(e) => {*/}
+        {/*        console.log(e.target.value)*/}
+        {/*    }*/}
+        {/*    }/>*/}
+
+        {/*방법 3*/}
         <input
             type="text"
             onChange={(e) => {
-                console.log(e.target.value)
+                const txt = e.target.value
+                showText(txt)
             }
             }/>
     </div>
