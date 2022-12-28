dcb472a47eead5146424271b587d0bd8d7ae4dcd
doyupKim <gltlvl12@gmail.com>
2022-12-28 14:57:49 +0900

    useState

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,47 +1,18 @@
+import {useState} from "react";
+
 export default function Hello() {
-    function showName() {
-        console.log("mike");
+    // let name = "Mike";
+    const [name, setName] = useState('Mike');
+
+    function changeName(){
+        setName(name === "Mike" ? "Jane" :"Mike")
     }
 
-    function showAge(age) {
-        console.log(age);
-    }
-
-    // function showText(e){
-    //     console.log(e.target.value)
-    // }
-
-    function showText(txt){
-        console.log(txt)
-    }
 
     return <div>
-        <h1>Hello</h1>
-        <button onClick={showName}>Show name</button>
-        <button
-            onClick={() => {
-                showAge(20)
-            }
-            }>Show age
-        </button>
-        {/*방법1*/}
-        {/*<input type="text" onChange={showText}/>*/}
+        <h1>state</h1>
+        <h2 id="name">{name}</h2>
+        <button onClick={changeName}>Change</button>
 
-        {/*방법 2*/}
-        {/*<input*/}
-        {/*    type="text"*/}
-        {/*    onChange={(e) => {*/}
-        {/*        console.log(e.target.value)*/}
-        {/*    }*/}
-        {/*    }/>*/}
-
-        {/*방법 3*/}
-        <input
-            type="text"
-            onChange={(e) => {
-                const txt = e.target.value
-                showText(txt)
-            }
-            }/>
     </div>
 }
