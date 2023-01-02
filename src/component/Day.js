336ad745932b6cfd347aeba3419189ac24fb5c6a
doyupKim <gltlvl12@gmail.com>
2023-01-02 21:56:01 +0900

    fetch 비동기 api 호출

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,18 +1,33 @@
-import dummy from "../db/data.json"
 import { useParams} from "react-router-dom";
 import Word from "./Word";
+import {useEffect, useState} from "react";
 
 export default function Day(){
     const {day} = useParams();
-    const wordList = dummy.words.filter(word => (
-        word.day === day
-    ))
+    // const wordList = dummy.words.filter(word => (
+    //     word.day === day
+    // ))
+    const [words, setWords] = useState([]);
+
+    useEffect(()=> {
+        //api 비동기호출
+        fetch(`http://localhost:3001/words?day=${day}`) // promise 반환됨
+            .then(res => {
+                console.log(res)
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setWords(data)
+            })
+    }, [day])
+
 
     return <>
     <h2>Day {day}</h2>
     <table>
         <tbody>
-        {wordList.map(word=>(
+        {words.map(word=>(
             <Word word={word} key={word.id}></Word>
             ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,38 +1,30 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
-export default function DayList(){
+
+export default function DayList() {
     const [days, setDays] = useState([])
-    const [count, setCount] = useState(0)
 
-    function onclick(){
-        setCount(count+1)
-    }
-    function onclick2(){
-        setDays([
-            ...days,
-            {
-                id:Math.random(),
-                day:1,
-            }
-        ])
-    }
 
-    useEffect(()=>{
-        console.log("count Change")
-        //의존성 배열 api 호출 시 useEffect를 사용하려면 '[]'을 넣어준다.
-    },[count])
+    useEffect(() => {
+        //api 비동기호출
+        fetch('http://localhost:3001/days') // promise 반환됨
+            .then(res => {
+                console.log(res)
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+                setDays(data)
+            })
+    }, [])
 
     return (
-    <>
         <ul className="list_day">
-        {days.map(day => (
-        <li key={day.id}>
-            <Link to={`/day/${day.day}`} > Day : {day.day}</Link>
-        </li>
-        ))}
-    </ul>
-    <button onClick={onclick}>{count}</button>
-        <button onClick={onclick2}>Day Change</button>
-    </>
+            {days.map(day => (
+                <li key={day.id}>
+                    <Link to={`/day/${day.day}`}> Day : {day.day}</Link>
+                </li>
+            ))}
+        </ul>
     )
 }
